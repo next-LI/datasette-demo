fc42ea3f7ea3cf714d0deb2f645de74dee7f2506
Brandon Roberts <brandon@bxroberts.org>
2021-08-11 21:41:09 -0700

    Add user auth via django sidecar

---
+++ django/urls.py
@@ -0,0 +1,36 @@
+"""user_mgmt URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+import json
+from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
+
+
+def user_responder(request):
+    if not request.user or not request.user.is_authenticated:
+        return HttpResponse('{}', 'application/json')
+    user_str = json.dumps({
+        'username': request.user.username, 
+    })
+    return HttpResponse(user_str, 'application/json')
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django_registration.backends.activation.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path(r'user/?', user_responder),
+]
